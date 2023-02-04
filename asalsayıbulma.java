b1bf7b2e7ae22da75a5cc332636e4144adbe8227
meryem2137 <116557318+meryem2137@users.noreply.github.com>
2023-02-04 22:27:57 +0300

    Add files via upload

---
+++ asalsayıbulma.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+
+public class asalsayıbulma{
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        for(int i=2; i<=100; i++){
+            int total=0;
+            for(int k=1; k<=i; k++){
+                if(i%k==0){
+                total++;
+            }
+        }if(total<=2){
+                System.out.print(i);
+        }
+
+
+
+
+
+
+
+
+        }}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//www.patika.dev
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
